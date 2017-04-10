104997fad0cdd6794efc33b086ff7896d2ce2de1
Keawe <account@keawewong.com>
2017-04-10 14:17:30 -0400

    Step 3 - Printing with Paremeter. Completed.

---
+++ NodeHTTP_Param.js
@@ -0,0 +1,31 @@
+function getAndPrintHTML(host, path) {
+
+  // require the https module
+  var https = require('https');
+
+  //create the object to pass the url
+  var requestOptions = {
+    host: host,
+    path: path
+  };
+
+  //get the url and set character encoding
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
+
+    var myData = ""
+
+    response.on('data', function(data) {
+      myData += data;
+    });
+
+    response.on('end', function() {
+      console.log(`Display whole data stream. Length:, ${myData.length}\n${myData}\n`);
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+
+getAndPrintHTML('sytantris.github.io', '/http-examples/step1.html');
